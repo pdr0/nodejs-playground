beef1c9304d12f01ee147dc9a680359dc72d0f0f
pdr0 <pedropablo.selasgarcia@williamhill.com>
2019-05-21 10:57:36 +0200

    Routing definition

---
+++ routes.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const {url, method} = req;
+
+    if (url === '/') {
+        res.write('<html><head><title>Hi there!</title></head><body><h1>I love Node</h1><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body></html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            console.log(`Chunk: ${chunk}`);
+            body.push(chunk)
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('./logs/message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+
+    }
+};
+
+module.exports = requestHandler;
--- server.js
+++ server.js
@@ -1,37 +1,9 @@
-const fs = require('fs');
 const path = require('path');
 const http = require('http');
 const os = require('os');
+const routes = require('./routes');
 
 
-const requestListener = (req, res) => {
-    console.log(req.url, req.method, req.headers);
-    const {url, method} = req;
-    if (url === '/') {
-        res.write('<html><head><title>Hi there!</title></head><body><h1>I love Node</h1><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body></html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', chunk => {
-            console.log(`Chunk: ${chunk}`);
-            body.push(chunk)
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('./logs/message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-
-
-    }
-};
-
-const server = http.createServer(requestListener);
+const server = http.createServer(routes);
 
 server.listen('6969', 'localhost');
