5a8189412990d03af7aeaae79ff6080ff859e843
pdr0 <pedropablo.selasgarcia@williamhill.com>
2019-05-22 17:36:00 +0200

    Remove assessment 1

--- assignment-1/server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const config = require('./config');
-const { handler } = require('./routes');
-
-const server = http.createServer(handler);
-
-server.listen(config.port, config.host);
--- routes.js
+++ routes.js
@@ -1,30 +1,34 @@
-const fs = require('fs');
-
 const requestHandler = (req, res) => {
     const {url, method} = req;
 
     if (url === '/') {
-        res.write('<html><head><title>Hi there!</title></head><body><h1>I love Node</h1><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body></html>');
+        const response = '<html><head><title>Hi there!</title></head><body><h1>I love Node</h1><a href="/users">Users</a><form action="/create-user" method="POST"><input type="text" title="name" name="name" maxlength="100" /><button type="submit">Create User</button></form></body></html>';
+        res.write(response);
         return res.end();
     }
 
-    if (url === '/message' && method === 'POST') {
+    if (url === '/create-user' && method === 'POST') {
         const body = [];
         req.on('data', chunk => {
-            console.log(`Chunk: ${chunk}`);
-            body.push(chunk)
+            body.push(chunk);
         });
+
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFile('./logs/message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-
+            console.log(`Data Received: ${message}`);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        })
     }
+
+    if (url === '/users') {
+        const response = '<html><head><title>Hi there!</title></head><body><h1>Users List</h1><a href="/">Home</a><ul><li>Mark Gotera</li><li>Pierre de la font</li><li>Carla Mariscal</li><li>Timoteo Semilla</li><li>Sergay Noesmalo</li></ul></body></html>\'';
+        res.write(response);
+        return res.end();
+    }
+
 };
 
 module.exports = {
